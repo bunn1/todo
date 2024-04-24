78521ad14beed142f4191a451be4a5312f4a8c0c
bunn1 <nicjohbun@gmail.com>
2024-04-24 21:40:59 +0200

    import useState, useRef, FiCheckCircle Insert useState with toDoList, currentTask, editTask, reminder

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useState, useRef } from 'react';
+import { FiCheckCircle } from 'react-icons/fi';
 
 const ToDo = () => {
+  const [toDoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState('');
+  const [editTask, setEditTask] = useState(null);
+  const [reminder, setReminder] = useState(false);
+
   return <div></div>;
 };
 
