88c42e9c8a15eb9436709306a58e93279bd53e92
bunn1 <nicjohbun@gmail.com>
2024-04-24 21:49:59 +0200

    inserting functions - addTask, toggleReminder, inputTask

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { useState, useRef } from 'react';
 import { FiCheckCircle } from 'react-icons/fi';
 
+const inputTask = useRef(null);
+
+function addTask() {
+  // add task to list
+  setTodoList(...toDoList, { task: currentTask, completed: false, reminder });
+
+  // clear line
+  inputTask.current.value = '';
+  setCurrentTask('');
+  setReminder(!reminder);
+}
+
+const toggleReminder = () => {
+  setReminder(!reminder);
+};
+
 const ToDo = () => {
   const [toDoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState('');
