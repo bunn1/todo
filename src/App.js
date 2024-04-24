312742cca2c4e85aa055e47e82299c55e653a311
bunn1 <nicjohbun@gmail.com>
2024-04-24 19:34:29 +0200

    import routes import useState useState - email useState - loggedIn Inserting path to Home and login Making props email, loggedIn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [email, setEmail] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
