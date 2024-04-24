800fa5da3ff141a65404b84fb78ecb70f328ad62
bunn1 <nicjohbun@gmail.com>
2024-04-24 21:04:58 +0200

    functions validateEmail, validatePassword, onButtonClick and divs with inputs and values

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,70 @@ const Login = (props) => {
   const [passwordError, setPasswordError] = useState('');
   const navigate = useNavigate();
 
-  return <div></div>;
+  const validateEmail = (email) => {
+    if (!email.includes('@')) {
+      setEmailError('Please enter a valid email address.');
+      return false;
+    }
+    setEmailError('');
+    return true;
+  };
+
+  const validatePassword = (password) => {
+    if (password.length < 8) {
+      setPasswordError('Password must be at least 8 characters long.');
+      return false;
+    }
+    setPasswordError('');
+    return true;
+  };
+
+  const onButtonClick = () => {
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid = validatePassword(password);
+    if (isEmailValid && isPasswordValid) {
+      props.setLoggedIn(true);
+      props.setEmail(email);
+      navigate('/todo');
+    }
+  };
+
+  return (
+    <div className={'mainContainer'}>
+      <div className={'titleContainer'}>
+        <h1>Login To The ToDo App</h1>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          value={email}
+          placeholder="Enter your email here"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          className={'inputButton'}
+          type="button"
+          onClick={onButtonClick}
+          value={'Log in'}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
