f3b462f7d8713e394111b742efdcdaf48c4c053e
bunn1 <nicjohbun@gmail.com>
2024-04-24 20:56:13 +0200

    Function handleButtonClick with if loggeIn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { useNavigate }
+import { useNavigate } from 'react-router-dom';
 
-function Home(props) {
+const Home = (props) => {
   const { email, loggedIn, setLoggedIn } = props;
   const navigate = useNavigate();
 
+  const handleButtonClick = () => {
+    if (!loggedIn) {
+      navigate('/login');
+    } else {
+      setLoggedIn(false);
+      navigate('/');
+    }
+  };
+
   return (
     <div className="mainContainer">
-      <div className={titleContainer}>
-      <h1>Welcome!</h1>
+      <div className="">
+        <h1>Welcome!</h1>
       </div>
 
-    <div className={buttonContainer}>
-    <input
-    className={'inputButton'}
-    type="button"
-    onClick={handleButtonClick}
-    value={'To Do App Log In'} />
-    {loggedIn ? <div>Your email address is {email}</div> : <div />}
-    </div>
+      <div className="">
+        <input
+          className={'inputButton'}
+          type="button"
+          onClick={handleButtonClick}
+          value={'To Do App Log In'}
+        />
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
