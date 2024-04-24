4fa6de2a88ac97191ac962afbf4f096beaeed880
bunn1 <nicjohbun@gmail.com>
2024-04-24 20:56:50 +0200

    Creating Login Component Inserting useState, useNavigate

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useNavigate, useState } from 'react-router-dom';
+
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
+
+  return <div></div>;
+};
+
+export default Login;
