64686d37100647951719f377ce86f4e2f18ba986
bunn1 <nicjohbun@gmail.com>
2024-04-24 19:31:59 +0200

    Home component with classes and one prop

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="mainContainer">
+      <div className={titleContainer}></div>
+    </div>
+  );
+}
+
+export default Home;
