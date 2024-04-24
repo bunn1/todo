51cd6458ae7473235c49733f6e575a50227718a9
bunn1 <nicjohbun@gmail.com>
2024-04-24 20:36:30 +0200

    inserting props - email, loggedIn, setLoggedIn useNavigate inserting divs with classname input with button, onClick

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useNavigate }
 
-function Home() {
+function Home(props) {
+  const { email, loggedIn, setLoggedIn } = props;
+  const navigate = useNavigate();
+
   return (
     <div className="mainContainer">
-      <div className={titleContainer}></div>
+      <div className={titleContainer}>
+      <h1>Welcome!</h1>
+      </div>
+
+    <div className={buttonContainer}>
+    <input
+    className={'inputButton'}
+    type="button"
+    onClick={handleButtonClick}
+    value={'To Do App Log In'} />
+    {loggedIn ? <div>Your email address is {email}</div> : <div />}
+    </div>
     </div>
   );
 }
