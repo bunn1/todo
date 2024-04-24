de97b8ca15a726b3744c892c134e46579fca58e7
bunn1 <nicjohbun@gmail.com>
2024-04-24 21:29:56 +0200

    import useState and useNavigate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate, useState } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
   const [email, setEmail] = useState('');
