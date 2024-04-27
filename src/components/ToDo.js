fcf2402137fd1fed4dfd8e37c23cfb7a7ebdd292
bunn1 <nicjohbun@gmail.com>
2024-04-27 20:50:16 +0200

    moving functions and making handleEdit function

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,35 +2,43 @@ import React from 'react';
 import { useState, useRef } from 'react';
 import { FiCheckCircle } from 'react-icons/fi';
 
-const inputTask = useRef(null);
-
-function addTask() {
-  // add task to list
-  setTodoList(...toDoList, { task: currentTask, completed: false, reminder });
-
-  // clear line
-  inputTask.current.value = '';
-  setCurrentTask('');
-  setReminder(!reminder);
-}
-
-const toggleReminder = () => {
-  setReminder(!reminder);
-};
-
-const deleteTask = (taskToDelete) => {
-  setTodoList(
-    toDoList.filter((task) => {
-      return task.task !== taskToDelete;
-    })
-  );
-};
-
 const ToDo = () => {
   const [toDoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState('');
   const [editTask, setEditTask] = useState(null);
   const [reminder, setReminder] = useState(false);
+
+  const inputTask = useRef(null);
+
+  function addTask() {
+    // add task to list
+    setTodoList(...toDoList, { task: currentTask, completed: false, reminder });
+
+    // clear line
+    inputTask.current.value = '';
+    setCurrentTask('');
+    setReminder(!reminder);
+  }
+
+  const toggleReminder = () => {
+    setReminder(!reminder);
+  };
+
+  const deleteTask = (taskToDelete) => {
+    setTodoList(
+      toDoList.filter((task) => {
+        return task.task !== taskToDelete;
+      })
+    );
+  };
+
+  const handleEdit = (task) => {
+    const foundTask = toDoList.find((t) => t.task === task);
+    if (foundTask) {
+      setEditTask(foundTask);
+      setCurrentTask(foundTask.task);
+    }
+  };
 
   return (
     <div className="App">
