a59eea1a7d66f771c8b03ea51ac33c180816e89c
bunn1 <nicjohbun@gmail.com>
2024-04-29 20:28:25 +0200

    creating a div

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -66,63 +66,65 @@ const ToDo = () => {
   };
 
   return (
-    <div className="App">
-      <h1>To Do List!</h1>
-      <div className="container">
-        <input
-          ref={inputTask}
-          value={currentTask}
-          type="text"
-          placeholder="..Task"
-          // Tryck på enter knappen för att lägga till en task
-          onKeyDown={(event) => {
-            event.keyCode === 13 && addTask();
-          }}
-          onChange={(event) => {
-            setCurrentTask(event.target.value);
-          }}
-        />
-        <label className="labelReminder">Set Reminder:</label>
-        <div className="form-control form-control-check">
+    <div>
+      <div className="App">
+        <h1>To Do List!</h1>
+        <div className="container">
           <input
-            className="checkReminder"
-            name="field-name-three"
-            id="unique-field-id-three"
-            type="checkbox"
-            checked={reminder}
-            // value={reminder}
-            onChange={toggleReminder}
+            ref={inputTask}
+            value={currentTask}
+            type="text"
+            placeholder="..Task"
+            // Tryck på enter knappen för att lägga till en task
+            onKeyDown={(event) => {
+              event.keyCode === 13 && addTask();
+            }}
+            onChange={(event) => {
+              setCurrentTask(event.target.value);
+            }}
           />
+          <label className="labelReminder">Set Reminder:</label>
+          <div className="form-control form-control-check">
+            <input
+              className="checkReminder"
+              name="field-name-three"
+              id="unique-field-id-three"
+              type="checkbox"
+              checked={reminder}
+              // value={reminder}
+              onChange={toggleReminder}
+            />
+          </div>
+
+          <button id="addButton" onClick={addTask}>
+            Add Task
+          </button>
         </div>
+        <hr />
 
-        <button id="addButton" onClick={addTask}>
-          Add Task
-        </button>
+        <ul>
+          {toDoList.map((val, key) => {
+            return (
+              <div id="task" key={key}>
+                <li
+                  style={{
+                    textDecoration: val.completed ? 'line-through' : 'none',
+                  }}
+                >
+                  {val.task}
+                  {val.reminder ? <FiCheckCircle /> : ''}
+                </li>
+                <button onClick={() => completeTask(val.task)}>
+                  {val.completed ? 'Mark Incomplete' : 'Mark Complete'}
+                </button>
+                <button onClick={() => deleteTask(val.task)}>X</button>
+                <button onClick={() => handleEdit(val.task)}>Edit</button>
+                <button onClick={handleSave}>Save</button>
+              </div>
+            );
+          })}
+        </ul>
       </div>
-      <hr />
-
-      <ul>
-        {toDoList.map((val, key) => {
-          return (
-            <div id="task" key={key}>
-              <li
-                style={{
-                  textDecoration: val.completed ? 'line-through' : 'none',
-                }}
-              >
-                {val.task}
-                {val.reminder ? <FiCheckCircle /> : ''}
-              </li>
-              <button onClick={() => completeTask(val.task)}>
-                {val.completed ? 'Mark Incomplete' : 'Mark Complete'}
-              </button>
-              <button onClick={() => deleteTask(val.task)}>X</button>
-              <button onClick={() => handleEdit(val.task)}>Edit</button>
-              <button onClick={handleSave}>Save</button>
-            </div>
-          );
-        })}
-      </ul>
     </div>
   );
 };
