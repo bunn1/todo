707f41bf727697f2509c6c238692c07a1b7ec05e
bunn1 <nicjohbun@gmail.com>
2024-04-30 13:16:24 +0200

    import index.css .App - outcomment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
 import Home from './components/Home.js';
